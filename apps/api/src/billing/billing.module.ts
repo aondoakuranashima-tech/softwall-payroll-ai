9f417608f3f014f56934a8b2ab9579011c326a21
aondoakuranashima-tech <aondoakuranashima@gmail.com>
2026-08-18 10:51:18 +0100

    feat(billing): register Stripe and Paystack providers

--- apps/api/src/billing/billing.module.ts
+++ apps/api/src/billing/billing.module.ts
@@ -1,1 +1,12 @@
-import {Module} from '@nestjs/common'; import {BillingController} from './billing.controller'; import {BillingService} from './billing.service'; @Module({controllers:[BillingController],providers:[BillingService]}) export class BillingModule {}
+import { Module } from '@nestjs/common';
+import { BillingController } from './billing.controller';
+import { BillingService } from './billing.service';
+import { StripeService } from './stripe.service';
+import { PaystackService } from './paystack.service';
+
+@Module({
+  controllers: [BillingController],
+  providers: [BillingService, StripeService, PaystackService],
+  exports: [BillingService, StripeService, PaystackService],
+})
+export class BillingModule {}
